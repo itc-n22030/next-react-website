c5410db87f882f39609e5f31808731f4961477ca
itc-n22030 <n22030@std.it-college.ac.jp>
2023-01-12 07:59:41 +0000

    更新

--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,24 @@ import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
+import Image from 'next/legacy/image'
+import eyecatch from 'images/about.jpg'
 
 export default function About () {
   return (
     <Container>
       <Hero title='About' subtitle='About development activities' />
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=''
+          layout='responsive'
+          sizes='(min-width: 1152px) 1152px, 100vw'
+          priority
+          placeholder='blur'
+        />
+
+      </figure>
 
       <TwoColumn>
         <TwoColumn.Main>
