9807994b02ba66a09579cb832504b1ca9b2facc4
itc-n22030 <n22030@std.it-college.ac.jp>
2023-01-13 02:21:19 +0000

    更新

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import 'styles/globals2.css'
 import Layout from 'components/layout'
 
+// font Awesome の設定
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 function MyApp ({ Component, pageProps }) {
   return (
     <Layout>
