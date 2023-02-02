cd3b6c57aa897cb8d7b4a6103a52012a874f2105
itc-n22030 <n22030@std.it-college.ac.jp>
2023-02-02 06:26:57 +0000

    8.2

--- components/convert-date.js
+++ components/convert-date.js
@@ -1,5 +1,5 @@
 import { parseISO, format } from 'date-fns'
-import ja from 'date-fns/locate/ja'
+import ja from 'date-fns/locale/ja'
 
 export default function ConverDate ({ dateISO }) {
   return (
