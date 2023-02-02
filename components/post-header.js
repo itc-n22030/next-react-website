b4567d1cdaa1ae4f85096bbf35e834f68fe317d5
itc-n22030 <n22030@std.it-college.ac.jp>
2023-02-02 06:06:49 +0000

    8.2まで

--- components/post-header.js
+++ components/post-header.js
@@ -1,6 +1,6 @@
 import styles from 'styles/post-header.module.css'
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
-import { faClock } from '@fontawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
 
 export default function PostHeader ({ title, subtitle, publish = '' }) {
   return (
