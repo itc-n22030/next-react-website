ef317a1b8f2f03cfc1a889022c6962294f58626e
itc-n22030 <n22030@std.it-college.ac.jp>
2023-02-02 06:35:26 +0000

    8.3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domain: ['iamges.microcms-assets.io']
+  }
 }
 
 module.exports = nextConfig
